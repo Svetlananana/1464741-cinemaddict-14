d27f3201f230376babf61b4890d95fb0aef55843
poison_ivy <hellosvetasmirnova@gmail.com>
2021-04-12 12:13:04 +0300

    Дописала функцию генерации времени

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,13 +1,15 @@
 import dayjs from 'dayjs';
-
-export const generateDate = (date) => {
-  return dayjs(date).format('«DD MMMM YYYY»');
-};
+import { getRandomNumber } from './random.js';
 
 export const genetateRunTime = (time) => {
-  return dayjs(time).format('«HH:mm»');
+  return dayjs(time).format('HH:mm');
 };
 
 export const generateDateComments = (date) => {
-  return dayjs(date).format('«DD MMMM YYYY HH:mm»');
+  return dayjs(date).format('YYYY/DD/MM HH:mm');
+};
+
+export const generateDate = (date) => {
+  const yearsGap = getRandomNumber(date, 0);
+  return dayjs().add(yearsGap, 'year').toDate();
 };
