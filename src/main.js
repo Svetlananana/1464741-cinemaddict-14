e715410c675130745f142f0c22aeae5d6012eb4b
poison_ivy <hellosvetasmirnova@gmail.com>
2021-04-12 12:20:17 +0300

    Вывела сообщение в случае отсутствия фильмов

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import { createProfileUser } from './view/profile-user.js';
 import { createMenuTemplate } from './view/menu.js';
 import { createSortList } from './view/sort.js';
 import { createFilmBlock } from './view/films.js';
-import { createFilmCards } from './view/film-cards.js';
+import { createFilmCard } from './view/film-cards.js';
 import { createShowMoreButton } from './view/show-more-button.js';
 import { createFilmDetails } from './view/film-details.js';
+import { createNoFilmsMessageTemplate } from './view/no-film-massage.js';
 import { generateFilmPoster } from './mock/film.js';
 import { render, InsertPlace } from './utils/render';
 
 const FILM_COUNT_LIST = 20;
 const FILM_COUNT_EXTRA = 2;
-const FILN_COUNT_STEP = 5;
+const FILM_COUNT_STEP = 5;
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
@@ -30,12 +31,16 @@ const mostCommented = filmsListExtra[1].querySelector('.films-list__container');
 
 const filmCards = new Array(FILM_COUNT_LIST).fill().map(generateFilmPoster);
 
-for (let i = 0; i < Math.min(filmCards.length, FILN_COUNT_STEP); i++) {
-  render(filmsListContainer, createFilmCards(filmCards[i]), InsertPlace.BEFORE_END);
+if (filmCards.length === 0) {
+  render(mainElement, createNoFilmsMessageTemplate(), InsertPlace.AFTER_BEGIN);
 }
 
-if (filmCards.length > FILN_COUNT_STEP) {
-  let renderCountStep = FILN_COUNT_STEP;
+for (let i = 0; i < Math.min(filmCards.length, FILM_COUNT_STEP); i++) {
+  render(filmsListContainer, createFilmCard(filmCards[i]), InsertPlace.BEFORE_END);
+}
+
+if (filmCards.length > FILM_COUNT_STEP) {
+  let renderCountStep = FILM_COUNT_STEP;
   render(filmsList, createShowMoreButton(), InsertPlace.BEFORE_END);
 
   const showMoreButton = films.querySelector('.films-list__show-more');
@@ -43,10 +48,10 @@ if (filmCards.length > FILN_COUNT_STEP) {
   showMoreButton.addEventListener('click', (evt) => {
     evt.preventDefault();
 
-    filmCards.slice(renderCountStep, renderCountStep + FILN_COUNT_STEP)
-      .forEach((filmCards) => render(filmsListContainer, createFilmCards(filmCards), InsertPlace.BEFORE_END));
+    filmCards.slice(renderCountStep, renderCountStep + FILM_COUNT_STEP)
+      .forEach((filmCards) => render(filmsListContainer, createFilmCard(filmCards), InsertPlace.BEFORE_END));
 
-    renderCountStep += FILN_COUNT_STEP;
+    renderCountStep += FILM_COUNT_STEP;
 
     if (renderCountStep >= filmCards.length) {
       showMoreButton.remove();
@@ -56,10 +61,10 @@ if (filmCards.length > FILN_COUNT_STEP) {
 }
 
 for (let i = 0; i < FILM_COUNT_EXTRA; i++) {
-  render(topRated, createFilmCards(filmCards[i]), InsertPlace.BEFORE_END);
+  render(topRated, createFilmCard(filmCards[i]), InsertPlace.BEFORE_END);
 }
 for (let i = 0; i < FILM_COUNT_EXTRA; i++) {
-  render(mostCommented, createFilmCards(filmCards[i]), InsertPlace.BEFORE_END);
+  render(mostCommented, createFilmCard(filmCards[i]), InsertPlace.BEFORE_END);
 }
 
 const body = document.querySelector('body');
