1f605d3996875f2fcd458f4c44fb2dddde081dc2
poison_ivy <hellosvetasmirnova@gmail.com>
2021-04-16 13:05:15 +0300

    Добавила словарь с датой и вынесла в константу тег body

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,15 +1,24 @@
 import dayjs from 'dayjs';
 import { getRandomNumber } from './random.js';
 
-export const genetateRunTime = (time) => {
-  return dayjs(time).format('HH:mm');
+const DateFormat = {
+  COMMENT : 'YYYY/DD/MM HH:mm',
+  RELEASE_YEAR: 'YYYY',
+  TIME: 'HH:mm',
 };
 
-export const generateDateComments = (date) => {
-  return dayjs(date).format('YYYY/DD/MM HH:mm');
+export const generateRunTime = (time = 0) => {
+  return dayjs(time).format(DateFormat.TIME);
 };
 
-export const generateDate = (date) => {
+export const generateDateComments = (date = 0) => {
+  return dayjs(date).format(DateFormat.COMMENT);
+};
+
+export const generateDate = (date = 0) => {
   const yearsGap = getRandomNumber(date, 0);
   return dayjs().add(yearsGap, 'year').toDate();
 };
+
+// Я говорил о том, чтобы хранить в date именно дату, не отформатированный вариант
+// А уже в представлении, когда будешь получать фильм через параметры, например, в createFilmTemplate приводить к нужному формату, а именно сделать функции
