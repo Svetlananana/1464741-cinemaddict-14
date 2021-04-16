a8680bf910069bdad933a468103313b29dd6de69
poison_ivy <hellosvetasmirnova@gmail.com>
2021-04-16 13:04:32 +0300

    Добавила словарь с датой и вынесла в константу тег body

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,6 +1,4 @@
-export const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+export const BODY = document.querySelector('body');
 
 export const InsertPlace = {
   BEFORE_END: 'beforeend',  //  сразу перед закрывающим тегом element (после последнего потомка)
@@ -8,3 +6,26 @@ export const InsertPlace = {
   BEFORE_BEGIN: 'beforebegin', // до самого element (до открывающего тега)
   AFTER_END: 'afterend', // после element (после закрывающего тега)
 };
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case InsertPlace.AFTER_BEGIN:
+      container.prepend(element);
+      break;
+    case InsertPlace.BEFORE_END:
+      container.append(element);
+      break;
+  }
+};
+
+// Принцип работы прост:
+// 1. создаём пустой div-блок
+// 2. берём HTML в виде строки и вкладываем в этот div-блок, превращая в DOM-элемент
+// 3. возвращаем этот DOM-элемент
+export const createElement = (template) => {
+  const newElement = document.createElement('div'); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstChild; // 3
+};
+
