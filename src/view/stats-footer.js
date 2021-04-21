bc64f26f11c8e35a42819431e67d29db77174ea8
poison_ivy <hellosvetasmirnova@gmail.com>
2021-04-22 02:11:36 +0300

    Добавила класс с выводом статистики в футер

---
+++ src/view/stats-footer.js
@@ -0,0 +1,13 @@
+import Abstract from './abstract.js';
+
+const createStatisticsTemplate = () => {
+  return `
+  <p>130 291 movies inside</p>`;
+};
+
+export default class Statistics extends Abstract{
+
+  getTemplate() {
+    return createStatisticsTemplate();
+  }
+}
