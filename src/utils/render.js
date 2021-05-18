af2cbf03d4fdd6023dd30fb573bb52477287e6a4
poison_ivy <hellosvetasmirnova@gmail.com>
2021-05-18 18:03:19 +0300

    Добавила утилитарную функцию updateItem и дописала функцию render

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,4 +1,4 @@
-export const BODY = document.querySelector('body');
+import Abstract from '../view/abstract.js';
 
 export const InsertPlace = {
   BEFORE_END: 'beforeend',  //  сразу перед закрывающим тегом element (после последнего потомка)
@@ -8,6 +8,14 @@ export const InsertPlace = {
 };
 
 export const render = (container, element, place) => {
+  if (container instanceof Abstract) {
+    container = container.getElement();
+  }
+
+  if (element instanceof Abstract) {
+    element = element.getElement();
+  }
+
   switch (place) {
     case InsertPlace.AFTER_BEGIN:
       container.prepend(element);
@@ -27,4 +35,49 @@ export const createElement = (template) => {
   newElement.innerHTML = template.trim(); // 2
 
   return newElement.firstChild; // 3
+};
+
+export const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
+export const remove = (component) => {
+  if (component === null) {
+    return;
+  }
+
+  if (!(component instanceof Abstract)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};
+
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
 };
