afd7c55db45290a7d466366f7699534ff9db8718
poison_ivy <hellosvetasmirnova@gmail.com>
2021-06-01 18:07:34 +0300

    Подключила модель

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import { generateFilm } from './mock/film.js';
 import MainPage from './presenter/main-page.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+
+import FilmsModel from './model/films.js';
+import FilterModel from './model/filter.js';
 
 const FILM_COUNT_LIST = 20;
 
 const bodyElement = document.querySelector('body');
+const mainElement = bodyElement.querySelector('main');
 
 const filmCards = new Array(FILM_COUNT_LIST).fill().map(generateFilm);
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(filmCards);
 
-const mainPagePresenter = new MainPage(bodyElement);
-mainPagePresenter.init(filmCards);
+const filterModel = new FilterModel();
+
+const mainPagePresenter = new MainPage(bodyElement, filmsModel, filterModel);
+const filterPresenter = new FilterPresenter(mainElement, filterModel, filmsModel);
+
+filterPresenter.init();
+mainPagePresenter.init();
